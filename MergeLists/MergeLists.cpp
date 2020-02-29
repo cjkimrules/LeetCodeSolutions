3bfc32f1034e36b262e8bd62405e811b0377e3f5
CJ Kim <cjkimrules@gmail.com>
2020-02-28 20:25:42 -0600

    added

---
+++ MergeLists/MergeLists.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include "MergeLists.h"
+#include "ListNode.h"
+
+using namespace std;
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* MergedList = l1;
+    // Two lists will be merged into one to "l1".
+    while(l2 != NULL){
+        // We will look through all "l2" components comparing with l1.
+        // Only way to put l2's component as "Next" of current "l1" is when that number
+        // is larger or equal to current value AND smaller than l1's next value.
+        if((l1->val <= l2->val) && (l1->next == NULL || (l1->next->val > l2->val))){
+            // If there is no "Next" value, then just add it as the largest number.
+            // If there is "Next" value, then make sure "l2" value is smaller.
+            // Add it.
+            ListNode* a = new ListNode(l2->val);
+            a->next = l1->next;
+            l1->next = a;
+            
+            // When we successfully added "l2" value into "l1", move to next value of "l2".
+            l2 = l2->next;
+        }else{
+            // If "l1"s Next wasn't null and it wasn't bigger than "l2",
+            // then move "l1" to next element.
+            // If "l1" value wasn't equal or smaller to "l2" then move to next element.
+            l1 = l1->next;
+        }
+    }
+    
+    return MergedList;
+}
+
---
+++ MergeLists/MergeLists.h
@@ -0,0 +1,8 @@
+#ifndef TEST_CASES
+#define TEST_CASES
+
+#include "ListNode.h"
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2);
+
+#endif
