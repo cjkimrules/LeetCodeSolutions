39f8dabeb8d1aaff3dcbc30ca30dcbb69e56c769
CJ Kim <cjkimrules@gmail.com>
2020-02-29 21:38:41 -0600

    done

--- strStr/main.cpp
+++ strStr/main.cpp
@@ -29,7 +29,13 @@ void String_toLower(string& input){
 // NOT-case sensitive. Aka, "Aba" will be treated the same as "aba".
 // Haystack: "Aba", needle: "ab", output: 0.
 
-// Time Complexity is the Number of Characters in Haystack.
+// Time Complexity is the (Size of Haystack) * (Size of Needle).
+// We are looping through only the Haystack, so it may seem like the time complexity is
+// (Size of Haystack), but that's not true for the worst case.
+// When there are many repetitive chars in Haystack (refer #9 test case),
+// we have to roll back Haystack Pointer (aka, "i" in for-loop), almost the size of needle.
+// Worst case, we might have to loop back for all characters in haystack, so the worst of worst
+// is (Size of Haystack) * (Size of Needle).
 
 int strStr(string haystack, string needle){
     if(needle == "") return 0;
@@ -57,7 +63,15 @@ int strStr(string haystack, string needle){
                 return NeedleIndex;
             }
         }else{
-            NeedlePointer = 0;
+            // When the characters turns out mismatched. We still have to make sure
+            // they are not repetitive letters. What I mean by that is
+            // haystack: "llove", needle: "love", output: 1.
+            // But it will output -1, not found, because once second "l" in haystack
+            // and "o" in needle mismatched, it proceeds. We have to reset haystack's pointer as well.
+            if(NeedlePointer != 0){
+                NeedlePointer = 0;
+                i = NeedleIndex;
+            }
         }
     }
 
@@ -82,4 +96,7 @@ int main(){
     Tester(4, "Not Found #1", "aaaaa", "ba", -1);
     Tester(5, "Not Found #2", "iloveyou", "loves", -1);
     Tester(6, "Needle is Larger", "YOU", "yours", -1);
+    Tester(7, "Repetitive #1", "llovesushi", "love", 1);
+    Tester(8, "Repetitive #2", "lolovesushi", "love", 2); 
+    Tester(9, "Repetitive #3", "aaaabbb", "aaab", 1); 
 }
